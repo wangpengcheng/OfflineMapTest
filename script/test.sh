aa985bf76bedaca08f201ce6cdd64a51f819eda7
wangpengcheng <37323208+wangpengcheng@users.noreply.github.com>
2020-09-14 21:02:47 +0800

    添加参数优化

--- script/test.sh
+++ script/test.sh
@@ -5,6 +5,7 @@
 for((;;)); do \
         ffmpeg -re -i /dev/video0 \
         -vcodec libx265 -acodec copy \
+        -tune zerolatency  -preset ultrafast \
         -rtsp_transport udp \
         -f rtsp -y rtsp://118.24.113.233:554/test; 
         sleep 1; \
